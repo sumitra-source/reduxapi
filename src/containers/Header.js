54701c9cadd7ed5e408fc1e77cdd2f1527a27263
Sumitra Shrestha <sumiitrashrestha@gmail.com>
2022-04-27 18:58:47 +0545

    redux with axiox api

---
+++ src/containers/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="ui fixed menu">
+      <div className="ui container center">
+       <center> <h2 >Online Store</h2></center>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
