6065528c232c47b065a09ab981b43a253241f236
sumitra <sumitraashrestha@gmail.com>
2022-06-29 18:13:45 +0545

    ui

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import store from "./redux/store";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(
-  <Provider store={store}>
-      <App />
-  </Provider>,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
